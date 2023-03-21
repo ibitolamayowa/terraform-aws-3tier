35a2b5bc9fb09c72f5448e3e59d8e9b3b598cf66
Mayowa Ibitola <mrdavidtola@gmail.com>
2023-03-21 13:22:52 +0100

    added application tier

--- app.tf
+++ app.tf
@@ -1,6 +1,6 @@
-# Create the security group for the web tier
-resource "aws_security_group" "web" {
-  name_prefix = "web-"
+# Create the security group for the app tier
+resource "aws_security_group" "app" {
+  name_prefix = "app-"
   vpc_id      = aws_vpc.aws_vpc.id
 
   ingress {
@@ -18,9 +18,9 @@ resource "aws_security_group" "web" {
   }
 
   ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
+    from_port = -1
+    to_port = -1
+    protocol = "icmp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -33,59 +33,39 @@ resource "aws_security_group" "web" {
 }
 
 # Create the launch configuration
-resource "aws_launch_configuration" "web" {
-  name                        = "web"
+resource "aws_launch_configuration" "app" {
+  name                        = "app"
   image_id                    = "ami-0df24e148fdb9f1d8"
   instance_type               = "t2.micro"
   key_name                    = "keypair"
-  security_groups             = [aws_security_group.web.id]
-  user_data                   = <<-EOF
-    #!/bin/bash
-    yum update -y
-    yum install -y httpd
-    systemctl start httpd
-    systemctl enable httpd
-    cd /var/www/html
-    echo "<h1>Company Website</h1>" > index.html
-  EOF
-  associate_public_ip_address = true
+  security_groups             = [aws_security_group.app.id]
+  associate_public_ip_address = false
 }
 
 # Create the application load balancer
-resource "aws_lb" "web" {
-  name            = "web"
-  internal        = false
+resource "aws_lb" "app" {
+  name            = "app"
+  internal        = true
   load_balancer_type = "application"
-  subnets         = aws_subnet.public[*].id
-
+  subnets            = [aws_subnet.private[0].id, aws_subnet.private[1].id]
   tags = {
-    Name = "web"
+    Name = "app"
   }
 }
 
 # Create the target group
-resource "aws_lb_target_group" "web" {
-  name_prefix     = "web"
+resource "aws_lb_target_group" "app" {
+  name_prefix     = "app"
   port            = 80
   protocol        = "HTTP"
   vpc_id          = aws_vpc.aws_vpc.id
-
-  health_check {
-    interval            = 10
-    path                = "/"
-    port                = 80
-    protocol            = "HTTP"
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-  }
 }
 
 # Create the autoscaling group
-resource "aws_autoscaling_group" "web" {
-  name                 = "aws_autoscaling_group_web"
-  launch_configuration       = aws_launch_configuration.web.name
-  vpc_zone_identifier         = aws_subnet.public[*].id
+resource "aws_autoscaling_group" "app" {
+  name                 = "aws_autoscaling_group_app"
+  launch_configuration       = aws_launch_configuration.app.name
+  vpc_zone_identifier         = [aws_subnet.private[0].id, aws_subnet.private[1].id]
   max_size                    = 2
   min_size                    = 1
   desired_capacity            = 1
@@ -94,11 +74,11 @@ resource "aws_autoscaling_group" "web" {
   termination_policies        = ["Default"]
   tag {
     key                 = "Environment"
-    value               = "Web"
+    value               = "app"
     propagate_at_launch = true
   }
 
-  target_group_arns = [aws_lb_target_group.web.arn]
+  target_group_arns = [aws_lb_target_group.app.arn]
 
   lifecycle {
     create_before_destroy = true
@@ -106,18 +86,18 @@ resource "aws_autoscaling_group" "web" {
 }
 
 # Create the scale up autoscaling policy
-resource "aws_autoscaling_policy" "scale-up" {
-  name = "autoscaling-policy-scaleup"
+resource "aws_autoscaling_policy" "app-scale-up" {
+  name = "autoscaling-policy-scaleup-app"
   policy_type = "SimpleScaling"
-  autoscaling_group_name  = aws_autoscaling_group.web.name
+  autoscaling_group_name  = aws_autoscaling_group.app.name
   adjustment_type = "ChangeInCapacity"
   scaling_adjustment = "1"
   cooldown = "300"
   }
 
 # Create the scale up cloudwatch alarm
-  resource "aws_cloudwatch_metric_alarm" "cpu-scale-up-alarm" {
-  alarm_name = "cpu-scale-up-alarm"
+  resource "aws_cloudwatch_metric_alarm" "cpu-scale-up-app-alarm" {
+  alarm_name = "cpu-scale-up-app-alarm"
   alarm_description = "cpu-scale-up-alarm"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods = "2"
@@ -127,24 +107,24 @@ resource "aws_autoscaling_policy" "scale-up" {
   statistic = "Average"
   threshold = "50"
   dimensions = {
-  "AutoScalingGroupName" = aws_autoscaling_group.web.name
+  "AutoScalingGroupName" = aws_autoscaling_group.app.name
   }
   alarm_actions = ["${aws_autoscaling_policy.scale-up.arn}"]
   }
 
 # Create the scale down autoscaling policy
-  resource "aws_autoscaling_policy" "scale-down" {
-name = "autoscaling-policy-scaledown"
+resource "aws_autoscaling_policy" "scale-down-app" {
+name = "autoscaling-policy-scaledown-app"
 policy_type = "SimpleScaling"
-autoscaling_group_name  = aws_autoscaling_group.web.name
+autoscaling_group_name  = aws_autoscaling_group.app.name
 adjustment_type = "ChangeInCapacity"
 scaling_adjustment = "-1"
 cooldown = "300"
 }
 
 # Create the scale down cloudwatch alarm
-resource "aws_cloudwatch_metric_alarm" "cpu-scale-down-alarm" {
-alarm_name = "cpu-scaledown-alarm"
+resource "aws_cloudwatch_metric_alarm" "cpu-scale-down-app-alarm" {
+alarm_name = "cpu-scaledown-app-alarm"
 comparison_operator = "LessThanOrEqualToThreshold"
 evaluation_periods = "2"
 metric_name = "CPUUtilization"
@@ -153,7 +133,11 @@ period = "10"
 statistic = "Average"
 threshold = "49"
 dimensions = {
-"AutoScalingGroupName" = aws_autoscaling_group.web.name
+"AutoScalingGroupName" = aws_autoscaling_group.app.name
 }
 alarm_actions = ["${aws_autoscaling_policy.scale-down.arn}"]
+}
+
+output "app_private_subnet_ids" {
+  value = [aws_subnet.private[0].id, aws_subnet.private[1].id]
 }
